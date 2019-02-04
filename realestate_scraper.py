9a53dd6185232a35c33078c9ceb7e709179c21ce
James Lovie <33210174+JamesLovie@users.noreply.github.com>
2019-02-04 16:51:10 +1100

    Update realestate_scraper.py

--- realestate_scraper.py
+++ realestate_scraper.py
@@ -28,7 +28,7 @@ date = str(date.today())
 try:
 
 	# Loop through first 50 pages each day and add results to dataframe at completion.
-	for count in range(1, 26):
+	for count in range(1, 51):
 
 		countstr = str(count)
 		page_link = 'https://www.realestate.com.au/buy/in-collingwood,+vic+3066%3b+st+kilda,+vic+3182%3b+st+kilda+east,+vic+3183%3b+st+kilda+west,+vic+3182%3b+fitzroy,+vic+3065%3b+fitzroy+north,+vic+3068%3b+clifton+hill,+vic+3068%3b+northcote,+vic+3070%3b+carlton+north,+vic+3054%3b+prahran,+vic+3181%3b+richmond,+vic+3121%3b+south+yarra,+vic+3141%3b+windsor,+vic+3181%3b+malvern,+vic+3144/list-' + countstr + '?includeSurrounding=true&activeSort=list-date&source=refinement'
